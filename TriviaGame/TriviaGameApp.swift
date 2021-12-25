94f07a570a14be472da30d93f4168d8e02497076
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-12-25 13:55:30 +0500

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Arkasha Zuev on 25.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
